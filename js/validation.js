853442d8b1848b85778d8d5011d362929aa9752b
JWEISSGOLD <JWEISSGOLD@GMAIL.com>
2019-04-05 08:31:16 -0400

    Mailchimp, Popup, Favicon Fix

--- js/validation.js
+++ js/validation.js
@@ -15,13 +15,17 @@ jQuery.noConflict();
         return this.optional(element) || /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(value);
     }, "Please enter valid phone number");
 
-        
-        
-        var form3 = "#formSub-platform";
-        $(form3).validate({
+        var form1 = "#pop-up-form-reg";
+        $(form1).validate({
             ignore: []
             , rules: {
-                EMAIL2: {
+                FNAME1: {
+                    required: true                    
+                }
+                , LNAME1: {
+                    required: true                   
+                }
+                , EMAIL1: {
                     required: true
                     , email: true
                 }
@@ -36,7 +40,80 @@ jQuery.noConflict();
                 $(element).removeClass(errorClass).addClass(validClass);
             }
             , messages: {
-                EMAIL: {
+                FNAME: {
+                    required: "Required field"
+                }
+                , LNAME: {
+                    required: "Required field"
+                    
+                }
+                , EMAIL: {
+                    required: "Required field",
+                    email: "Invalid email"
+                , }
+            }
+            , errorPlacement: function (error, element) {
+                error.insertAfter(element);
+            }
+            , submitHandler: function (form) {
+                $.post("pop-up-form-registration.php", {
+                    FNAME: $("#FNAME1").val()
+                    , LNAME: $("#LNAME1").val()
+                    , EMAIL: $("#EMAIL1").val(),
+                    // PHONE:$('#PHONE').val(),
+                }, function (data) {
+                    var data1 = $.parseJSON(data);
+                    console.log(data1);
+                    if(data1 == 'success')
+                        {
+                            $('#response1').html('<h4 class="thankyou_msg">Thank you, you have been added to our mailing list.</h4>');
+                            $('#pop-up-form-reg')[0].reset();
+                            setTimeout(function(){
+                                $(".thankyou_msg").fadeOut();
+                            },3000);
+                        }
+                    else
+                        {
+                            $('#response1').html('<h4>Please try again.</h4>');
+                        }
+                    
+                });
+            }
+        });
+
+        var form3 = "#formSub-platform";
+        $(form3).validate({
+            ignore: []
+            , rules: {
+                FNAME3: {
+                    required: true                    
+                }
+                , LNAME3: {
+                    required: true                   
+                }
+                , EMAIL3: {
+                    required: true
+                    , email: true
+                }
+            }
+            , errorClass: 'error'
+            , validClass: 'valid'
+            , errorElement: 'span'
+            , highlight: function (element, errorClass, validClass) {
+                $(element).addClass(errorClass).removeClass(validClass);
+            }
+            , unhighlight: function (element, errorClass, validClass) {
+                $(element).removeClass(errorClass).addClass(validClass);
+            }
+            , messages: {
+                FNAME: {
+                    required: "Required field"
+                }
+                , LNAME: {
+                    required: "Required field"
+                    
+                }
+                , EMAIL: {
                     required: "Required field",
                     email: "Invalid email"
                 , }
@@ -46,29 +123,38 @@ jQuery.noConflict();
             }
             , submitHandler: function (form) {
                 $.post("after-platform-registration.php", {
-                    EMAIL: $("#EMAIL2").val(),
+                    FNAME: $("#FNAME3").val()
+                    , LNAME: $("#LNAME3").val()
+                    , EMAIL: $("#EMAIL3").val(),
+                    // PHONE:$('#PHONE').val(),
                 }, function (data) {
                     var data1 = $.parseJSON(data);
                     console.log(data1);
                     if(data1 == 'success')
                         {
-                            $('#response2').html('<h4>Thank you, you have been added to our mailing list.</h4>');
+                            $('#response3').html('<h4>Thank you, you have been added to our mailing list.</h4>');
                             $('#formSub-platform')[0].reset();
                         }
                     else
                         {
-                            $('#response2').html('<h4>Please try again.</h4>');
+                            $('#response3').html('<h4>Please try again.</h4>');
                         }
                     
                 });
             }
         });
 
-        var form4 = "#formSub-meet-team";
+       var form4 = "#formSub-meet-team";
         $(form4).validate({
             ignore: []
             , rules: {
-                EMAIL3: {
+                FNAME4: {
+                    required: true                    
+                }
+                , LNAME4: {
+                    required: true                   
+                }
+                , EMAIL4: {
                     required: true
                     , email: true
                 }
@@ -83,7 +169,14 @@ jQuery.noConflict();
                 $(element).removeClass(errorClass).addClass(validClass);
             }
             , messages: {
-                EMAIL: {
+                FNAME: {
+                    required: "Required field"
+                }
+                , LNAME: {
+                    required: "Required field"
+                    
+                }
+                , EMAIL: {
                     required: "Required field",
                     email: "Invalid email"
                 , }
@@ -93,18 +186,21 @@ jQuery.noConflict();
             }
             , submitHandler: function (form) {
                 $.post("meet-fam-registration.php", {
-                    EMAIL: $("#EMAIL3").val(),
+                    FNAME: $("#FNAME4").val()
+                    , LNAME: $("#LNAME4").val()
+                    , EMAIL: $("#EMAIL4").val(),
+                    // PHONE:$('#PHONE').val(),
                 }, function (data) {
                     var data1 = $.parseJSON(data);
                     console.log(data1);
                     if(data1 == 'success')
                         {
-                            $('#response3').html('<h4>Thank you, you have been added to our mailing list.</h4>');
+                            $('#response4').html('<h4>Thank you, you have been added to our mailing list.</h4>');
                             $('#formSub-meet-team')[0].reset();
                         }
                     else
                         {
-                            $('#response3').html('<h4>Please try again.</h4>');
+                            $('#response4').html('<h4>Please try again.</h4>');
                         }
                     
                 });
